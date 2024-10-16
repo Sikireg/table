85f20eb5ab67fba37e141c9d79c70e27c12473f1
Sikireg <masj280906@gmail.com>
2024-10-16 11:05:54 +0300

    Func new in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
+void delet(int** t, size_t m, size_t n);
+int ** ct_table(size_t m,size_t n);
 int main(){
   size_t M=0,N = 0;
   std::cin >> M >>N;
+  int ** t = ct_table(M,N);
+  delet(t,M,N);
 }
