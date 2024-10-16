1573390444da9f67ecdd722c9a42ef21cf65dd5d
Sikireg <masj280906@gmail.com>
2024-10-16 11:54:22 +0300

    very good commit

--- matrix.cpp
+++ matrix.cpp
@@ -21,3 +21,17 @@ for (size_t i = 0; i<m; ++i){
   }
 delete [] t;
 }
+int ** cr_table(size_t m,size_t n){
+  int** t = new int*[m];
+  size_t created = 0;
+  try{
+    for(;created<n;++created){
+      t[created0 = new int[n];
+    }
+  }
+  catch(const std::bad.alloc & e){
+    dt(t,created);
+    throw;
+  }
+  return t
+}
