cc612b23d756a3171efe283bd1986eec56aa96a0
Sikireg <masj280906@gmail.com>
2024-10-18 22:14:35 +0300

    Norm version without check

--- matrix.cpp
+++ matrix.cpp
@@ -1,15 +1,15 @@
 #include "matrix.hpp"
 void inn(int** t,size_t m, size_t n){
-  for (size_t i = 0; i<M;i++){
-    for (size_t j = 0; j<N;j++){
+  for (size_t i = 0; i<m;i++){
+    for (size_t j = 0; j<n;j++){
       std::cin>>t[i][j];
     }
   }
 }
 void out(const int* const* t,size_t m, size_t n){
-  for(size_t i = 0; i<M; i++){
+  for(size_t i = 0; i<m; i++){
     std::cout<<t[i][0];
-    for (size_t j = 1; j<N; j++){
+    for (size_t j = 1; j<n; j++){
       std::cout << " "<<t[i][j];
     }
     std::cout<<"\n";
@@ -33,5 +33,5 @@ int ** cr_table(size_t m,size_t n){
     dt(t,created);
     throw;
   }
-  return t
+  return t;
 }
--- matrix.hpp
+++ matrix.hpp
@@ -1,6 +1,7 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 #include <cstddef>
+#include <iostream>
 void delet(int** t, size_t m, size_t n);
 int ** ct_table(size_t m,size_t n);
 void inn(int** t,size_t m, size_t n);
