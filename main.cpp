abfd3161d49bd9dce2e3de4b191b636d57e270bb
Sikireg <masj280906@gmail.com>
2024-10-19 18:46:32 +0300

    Near-final

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 int main(){
   size_t M=0,N = 0;
   std::cin >> M >>N;
-  int ** t = ct_table(M,N);
+  if (!std::cin){
+    std::cerr<<"Not a number\n";
+    return 1;
+  }
+  try{
+    int ** t = ct_table(M,N);
+  }
+  catch(const std::bad_alloc & e){
+    std::cerr<<"Out of memory\n";
+    return 1;
+  };
+  inn(t,M,N);
+  out(t,M,N);  
   delet(t,M,N);
 }
 
--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,5 @@
 #include "matrix.hpp"
+#include <iostream>
 void inn(int** t,size_t m, size_t n){
   for (size_t i = 0; i<m;i++){
     for (size_t j = 0; j<n;j++){
