b151be74e54be300107519d78c743cd8da886927
Sikireg <masj280906@gmail.com>
2024-10-19 21:45:46 +0300

    Last version file with func

--- matrix.cpp
+++ matrix.cpp
@@ -22,7 +22,7 @@ for (size_t i = 0; i<m; ++i){
   }
 delete [] t;
 }
-int ** cr_table(size_t m,size_t n){
+int ** ct_table(size_t m,size_t n){
   int** t = new int*[m];
   size_t created = 0;
   try{
